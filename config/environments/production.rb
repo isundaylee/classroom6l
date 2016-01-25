bd854025b5f4937e25d49e554aeb7f74b633029d
Jiahao Li <isundaylee.reg@gmail.com>
2016-01-25 12:42:16 +0800

    Action Cable for dokku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.use ActionCableProxy
+  config.web_socket_server_url = "wss://6l.jiahao.link"
 end
